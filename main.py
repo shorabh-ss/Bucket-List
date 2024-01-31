8e8d778a807ed2fdfdbb98abb6d539951a6e5437
Mcc <roronauzumaki@gmail.com>
2024-01-30 23:39:26 -0800

    add session_key

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask ,render_template,request,json,jsonify,redirect
+from flask import Flask ,render_template,request,json,jsonify,redirect,session
+import os
+from werkzeug.security import check_password_hash
 
 import mysql.connector
 from mysql_conn import create_user
 
 app =Flask(__name__,template_folder="templates")
+
+app.secret_key=os.urandom(25)
 
 
 def mysql_con():
@@ -24,6 +28,12 @@ def index():
 def sign_up():
     return render_template('signup.html')
 
+@app.route("/home")
+def home():
+    if 'user_id' in session:
+        return render_template("homepage.html")
+    else:
+        return redirect("/signin")
 
 
 @app.route('/signin')
@@ -48,7 +58,23 @@ def create():
     return redirect("/signin")
 
 
+@app.route("/api/validateLogin", methods=['POST'])
+def check():
+    _email=request.form.get("_inputEmail")
+    _password=request.form.get("_inputPassword")
 
+    conn, cursor = mysql_con()
+    print("Input Email:", _email)
+    print("Input Password:", _password)
+    cursor.execute("SELECT * FROM tbl_user WHERE user_username LIKE %s AND user_password LIKE %s", (_email, _password))
+    
+    user= cursor.fetchone()
+    print(user)
+    if len(user) > 0:
+        session['user_id'] = user[0]
+        return redirect("/home")
+    else:
+        return redirect("/signin")
 
 
 
